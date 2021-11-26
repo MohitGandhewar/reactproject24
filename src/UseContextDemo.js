a84e73f8d60f098d2e1f8685f8a41139af91d4f5
Abhi <abhijeetvc7@gmail.com>
2021-11-26 21:23:08 +0530

    UseContext

--- src/UseContextDemo.js
+++ src/UseContextDemo.js
@@ -5,45 +5,54 @@ const ContextValue=createContext()
 
 function UseContextDemo(){
 
-    const value=1100
+    //const value=1100
+
+    const[value,setValue]=React.useState(1000)
+    const[value1,setValue1]=React.useState(2000)
+    const changeValue=()=>{
+        setValue(value+100)
+    }
 
     return(
-      <ContextValue.Provider value={value}>
+      <ContextValue.Provider value={{value,value1,changeValue}}>
           <h1>Context Demo</h1>
           <CheckContext/>
-          <CheckContext1/>
+           <CheckContext1/> 
       </ContextValue.Provider>  
         
     )
-
 }
-
 
 function CheckContext(){
 
-    const gValue=React.useContext(ContextValue)
-
+    const {value,value1,changeValue}=React.useContext(ContextValue)
+    console.log(value);
     return(
         // <ContextValue.Consumer>
         //     {value=><h2>Value is: {value}</h2>}
         // </ContextValue.Consumer>
 
         <div>
-            <h2>Value is: {gValue}</h2>
+            <h2>Value is: {value}</h2>
+            <h2>Value1 is: {value1}</h2>
+            <button type='button' onClick={changeValue}>Component1</button>
         </div>
     )
 }
 
 function CheckContext1(){
 
-    const gValue=React.useContext(ContextValue)
+    const {value,value1,changeValue}=React.useContext(ContextValue)
     return(
         // <ContextValue.Consumer>
         //     {value=><h2>Value is: {value}</h2>}
         // </ContextValue.Consumer>
 
         <div>
-            <h2>Value is: {gValue}</h2>
+            <h2>Value is: {value}</h2>
+            <h2>Value1 is: {value1}</h2>
+            <button type='button' onClick={changeValue}>Component2</button>
+
         </div>
     )
 }
